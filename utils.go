3786bca00dd1fd9ebaf83ef82295eaadac89e517
JinnyYi <82445294+JinnyYi@users.noreply.github.com>
2021-06-01 10:28:44 +0800

    Implement GSP-87 Feature Gates (#31)

--- utils.go
+++ utils.go
@@ -29,6 +29,7 @@ type Service struct {
 	projectID string
 
 	defaultPairs DefaultServicePairs
+	features     ServiceFeatures
 
 	typ.UnimplementedServicer
 }
@@ -46,7 +47,7 @@ type Storage struct {
 	workDir string
 
 	defaultPairs DefaultStoragePairs
-	pairPolicy   typ.PairPolicy
+	features     StorageFeatures
 
 	typ.UnimplementedStorager
 }
@@ -135,6 +136,9 @@ func newServicer(pairs ...typ.Pair) (srv *Service, err error) {
 	if opt.HasDefaultServicePairs {
 		srv.defaultPairs = opt.DefaultServicePairs
 	}
+	if opt.HasServiceFeatures {
+		srv.features = opt.ServiceFeatures
+	}
 	return
 }
 
@@ -206,8 +210,8 @@ func (s *Service) newStorage(pairs ...typ.Pair) (st *Storage, err error) {
 	if opt.HasDefaultStoragePairs {
 		store.defaultPairs = opt.DefaultStoragePairs
 	}
-	if opt.HasPairPolicy {
-		store.pairPolicy = opt.PairPolicy
+	if opt.HasStorageFeatures {
+		store.features = opt.StorageFeatures
 	}
 	if opt.HasWorkDir {
 		store.workDir = opt.WorkDir
