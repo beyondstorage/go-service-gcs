55f850e0b62e7bca4315ee612e902ab2bdd25d54
abyss-w <mad.hatter@foxmail.com>
2021-09-24 16:49:37 +0800

    feat: Add support for Write Empty File Behavior

--- storage.go
+++ storage.go
@@ -1,8 +1,10 @@
 package gcs
 
 import (
+	"bytes"
 	"context"
 	"errors"
+	"fmt"
 	"io"
 
 	gs "cloud.google.com/go/storage"
@@ -219,6 +221,16 @@ func (s *Storage) stat(ctx context.Context, path string, opt pairStorageStat) (o
 }
 
 func (s *Storage) write(ctx context.Context, path string, r io.Reader, size int64, opt pairStorageWrite) (n int64, err error) {
+	// According to GSP-751, we should allow the user to pass in a nil io.Reader.
+	// ref: https://github.com/beyondstorage/go-storage/blob/master/docs/rfcs/751-write-empty-file-behavior.md
+	if r == nil && size == 0 {
+		r = bytes.NewReader([]byte{})
+	} else if r == nil && size != 0 {
+		return 0, fmt.Errorf("reader is nil but size is not 0")
+	} else {
+		r = io.LimitReader(r, size)
+	}
+
 	rp := s.getAbsPath(path)
 	object := s.bucket.Object(rp)
 	if opt.HasEncryptionKey {
